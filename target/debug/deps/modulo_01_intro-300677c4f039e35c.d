81771a0519794a6f861855cd23337e1b24311450
Amsterdan Vasconcelos <amsterdan.rust@gmail.com>
2026-05-22 15:21:34 -0300

    chore: configure workspace package metadata and cargo aliases

---
+++ target/debug/deps/modulo_01_intro-300677c4f039e35c.d
@@ -0,0 +1,5 @@
+/home/amsterdan/developer/workspace-axum/target/debug/deps/modulo_01_intro-300677c4f039e35c.d: modulo-01-intro/src/main.rs
+
+/home/amsterdan/developer/workspace-axum/target/debug/deps/libmodulo_01_intro-300677c4f039e35c.rmeta: modulo-01-intro/src/main.rs
+
+modulo-01-intro/src/main.rs:
---
+++ target/debug/deps/modulo_01_intro-ea12af5762ab9f5d.d
@@ -0,0 +1,5 @@
+/home/amsterdan/developer/workspace-axum/target/debug/deps/modulo_01_intro-ea12af5762ab9f5d.d: modulo-01-intro/src/main.rs
+
+/home/amsterdan/developer/workspace-axum/target/debug/deps/libmodulo_01_intro-ea12af5762ab9f5d.rmeta: modulo-01-intro/src/main.rs
+
+modulo-01-intro/src/main.rs:
